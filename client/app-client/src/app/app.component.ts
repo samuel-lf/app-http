c2455ab1db5c6cb565d6b150d968c9e2f341327f
Samuel Leonel <samueleonelfreitas@hotmail.com>
2019-05-21 22:55:48 -0300

    Criação de uma requisição simples

--- client/app-client/src/app/app.component.ts
+++ client/app-client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Product } from './models/product.model';
+import { ProductsService } from './products.service';
+import { Component, OnInit } from '@angular/core';
+import { Observer, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-client';
+export class AppComponent implements OnInit {
+
+  simpleReqProductsObs$: Observable<Product[]>;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+  }
+
+  getSimpleHttpRequest() {
+    this.simpleReqProductsObs$ =  this.productsService.getProducts();
+  }
 }
